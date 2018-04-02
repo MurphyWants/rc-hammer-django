15c1df1d42ee75fa6aebc2bb82bdc4e3c0cf1ab7
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 15:24:46 +0000

    Added static files to urls.....I don't think I need this. On the todo list

--- rc_server/urls.py
+++ rc_server/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(r'polls/', include('polls.urls')),
     path(r'admin/', admin.site.urls),
     path(r'', include('home.urls')),
     path(r'dashboard/', include('rc_car.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
