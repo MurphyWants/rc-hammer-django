8ec17d84e9048084e1e2e13ee91b357c02a72324
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 17:53:02 -0400

    moved urls

--- home/urls.py
+++ home/urls.py
@@ -16,8 +16,4 @@ urlpatterns = [
     path(r'Logout', auth_views.logout),
     path(r'register', views.register, name='register'),
     path(r'Register', views.register, name='register'),
-    path(r'password_reset', auth_views.password_reset, name='password_reset'),
-    path(r'password_reset/done', auth_views.password_reset_done, name='password_reset_done'),
-    path(r'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    path(r'reset/done', auth_views.password_reset_complete, name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- rc_server/urls.py
+++ rc_server/urls.py
@@ -18,10 +18,15 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 urlpatterns = [
     path(r'polls/', include('polls.urls')),
     path(r'admin/', admin.site.urls),
     path(r'', include('home.urls')),
     path(r'dashboard/', include('rc_car.urls'))
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
