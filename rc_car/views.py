762c11b09c5f9705c3264cc077352c0579c686f8
jonathan murphy <jonathan@jmurphy.me>
2018-04-02 12:19:04 -0400

    added shared cars

--- rc_car/views.py
+++ rc_car/views.py
@@ -8,8 +8,10 @@ from .models import RC_Car
 def index(request):
     template_name = 'dashboard/index.html'
     current_user = request.user
-    cars = RC_Car.objects.filter(owner__id=current_user.id)
-    return render(request, template_name, {'rc_car_owned_list' : cars})
+    cars_owned = RC_Car.objects.filter(owner__id=current_user.id)
+    cars_shared = RC_Car.objects.filter(viewer_list__id=current_user.id) | RC_Car.objects.filter(user_list__id=current_user.id)
+    return render(request, template_name, {'rc_car_owned_list' : cars_owned,
+                                            'rc_car_shared_list': cars_shared})
 
 
 def by_uuid(request, unique_id):
