11dd47a61e1499a3450e058d741ac91adf80ed8a
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 17:54:47 -0400

    path --> url

--- rc_server/urls.py
+++ rc_server/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path(r'polls/', include('polls.urls')),
     path(r'admin/', admin.site.urls),
     path(r'', include('home.urls')),
-    path(r'dashboard/', include('rc_car.urls'))
-    path(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    path(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    path(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    path(r'dashboard/', include('rc_car.urls')),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
