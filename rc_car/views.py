f37a0f4bf0c59dde8f41e40725a2bb04af758781
jonathan murphy <jonathan@jmurphy.me>
2018-04-02 11:49:55 -0400

    views, dashbaord should show all owned cars

--- rc_car/views.py
+++ rc_car/views.py
@@ -4,9 +4,13 @@ from .models import RC_Car
 
 # Create your views here.
 
+
 def index(request):
     template_name = 'dashboard/index.html'
-    return render(request, template_name)
+    current_user = request.user
+    cars = RC_Car.objects.filter(owner__id=current_user)
+    return render(request, template_name, {'rc_car_user_list' : cars})
+
 
 def by_uuid(request, unique_id):
     return 0
