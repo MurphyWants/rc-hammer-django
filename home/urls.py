64bf1df7290b2e31e7419c2e855849db0a4e25fd
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 17:23:22 -0400

    did not add url import

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.conf.urls import url
 
 from . import views
 
