8f1c6af0e324408b72f0798a9ecc07f72d6daf1b
jonathan murphy <jonathan@jmurphy.me>
2018-04-02 11:52:52 -0400

    fixed query to id, not lazy user model

--- rc_car/views.py
+++ rc_car/views.py
@@ -8,7 +8,7 @@ from .models import RC_Car
 def index(request):
     template_name = 'dashboard/index.html'
     current_user = request.user
-    cars = RC_Car.objects.filter(owner__id=current_user)
+    cars = RC_Car.objects.filter(owner__id=current_user.id)
     return render(request, template_name, {'rc_car_user_list' : cars})
 
 
