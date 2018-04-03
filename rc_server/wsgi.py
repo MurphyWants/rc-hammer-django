e9548b1130838211f9d60c888ca44c601023a3ab
jonathan murphy <jonathan@jmurphy.me>
2018-04-03 17:48:18 -0400

    updated wsgi to help point to correct place

--- rc_server/wsgi.py
+++ rc_server/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rc_server.settings")
 
 application = get_wsgi_application()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rc_server.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rc_server  .settings")
+
+'''
+https://stackoverflow.com/questions/36210686/importerror-no-module-named-mysite-settings-django/36211423
+
+Daphne gets error: ImportError: No module named 'rc_car.settings'
+
+'''
