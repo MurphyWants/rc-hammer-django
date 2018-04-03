6bcd6ef68f4c32fc19b704942be8fd475256ecf0
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-03 10:34:46 -0400

    updated forms to clean name

--- rc_car/forms.py
+++ rc_car/forms.py
@@ -3,7 +3,18 @@ from datetime import datetime
 from .models import RC_Car
 
 class New_Car(forms.ModelForm):
+    name = forms.CharField(max_length=200, strip=True, min_length=1)
+
     class Meta:
         model = RC_Car
         fields = ('name',)
         exclude = ('owner',)
+
+    def save(self, commit=True):
+        car = super(New_Car, self).save(commit=False)
+        car.name = self.cleaned_data['name']
+
+        if commit:
+            car.save()
+
+        return car
