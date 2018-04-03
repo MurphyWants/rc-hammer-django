8e7e672e142bd81c385416b27ccdb8be90d451bd
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 22:07:01 -0400

    Form ==> Modelform

--- rc_car/forms.py
+++ rc_car/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from datetime import datetime
 from .models import RC_Car
 
-class New_Car(forms.Form):
+class New_Car(forms.ModelForm):
     class Meta:
         model = RC_Car
         fields = ('name')
