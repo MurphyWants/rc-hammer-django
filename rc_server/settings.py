c3ff7c4299eb99783dc37cce1681f60aad6eff99
jonathan murphy <jonathan@jmurphy.me>
2018-04-03 16:28:57 -0400

    ditto

--- rc_server/settings.py
+++ rc_server/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
