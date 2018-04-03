19df8d195e0325bc2fd3e4ee006cd0a675023c9d
jonathan murphy <jonathan@jmurphy.me>
2018-04-03 16:37:06 -0400

    we do need daphne

--- rc_car/routing.py
+++ rc_car/routing.py
@@ -7,6 +7,8 @@ from rc_car.consumers import ws_connect, ws_disconenct, ws_recieve
     route('websocket.disconnect', ws_disconnect),
 ]'''
 
+ASGI_APPLICATION = "rc_car.routing.application"
+
 def message_handler(message):
     print(message['text'])
 
