ff08514d2a90d3923bbb1eac57bb0f769fe08594
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 21:59:22 -0400

    typo

--- rc_car/forms.py
+++ rc_car/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from datetime import datetime
 from .models import RC_Car
 
-class New_Car(forms.form):
+class New_Car(forms.Form):
     name = forms.CharField(required=True, max_length=200, strip=True).help_text = '<br/>Name your car!'
 
     class Meta:
