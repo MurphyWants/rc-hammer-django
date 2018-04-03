d97431be76fefa078788ae4c97f5c0c1d4480299
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 22:04:35 -0400

    removed name from form class

--- rc_car/forms.py
+++ rc_car/forms.py
@@ -3,8 +3,6 @@ from datetime import datetime
 from .models import RC_Car
 
 class New_Car(forms.Form):
-    name = forms.CharField(required=True, max_length=200, strip=True).help_text = '<br/>Name your car!'
-
     class Meta:
         model = RC_Car
         fields = ('name')
