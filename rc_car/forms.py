1223b638d38e7c497b0613cec86e090e1926f420
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-02 22:05:21 -0400

    meta class

--- rc_car/forms.py
+++ rc_car/forms.py
@@ -6,12 +6,3 @@ class New_Car(forms.Form):
     class Meta:
         model = RC_Car
         fields = ('name')
-
-    def save(self, commit = True):
-        car = super(New_Car, self).save(commit=False)
-        car.name = self.cleaned_data['name']
-
-        if commit:
-            user.save()
-
-        return car
