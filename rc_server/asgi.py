c108853725e1c33e5a7edd5f023c8ddba0a23f74
jonathan murphy <jonathan@jmurphy.me>
2018-04-03 18:18:49 -0400

    rc_car ==> rc_server

--- rc_server/asgi.py
+++ rc_server/asgi.py
@@ -7,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rc_car.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rc_server.settings")
 django.setup()
 application = get_default_application()
