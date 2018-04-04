44a16466666371e135f1a372cfa9a2338f24ecd9
jonathan murphy <jonathan@jmurphy.me>
2018-04-04 08:39:05 -0400

    removing, renaming

--- rc_server/routing.py
+++ rc_server/routing.py
@@ -1,4 +1,4 @@
-from channels import route
+'''from channels import route
 
 # This function will display all messages received in the console
 def message_handler(message):
@@ -7,9 +7,18 @@ def message_handler(message):
 
 channel_routing = [
     route("websocket.receive", message_handler)  # we register our message handler
-]
+]'''
 
 '''
 https://gearheart.io/blog/creating-a-chat-with-django-channels/
 https://codyparker.com/django-channels-with-react/2/
 '''
+
+
+# mysite/routing.py
+# http://channels.readthedocs.io/en/latest/tutorial/part_1.html
+from channels.routing import ProtocolTypeRouter
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+})
