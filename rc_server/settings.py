27b67d7f666a6540ac6c24fb7b2edf5ffe0cb5a0
jonathan murphy <jonathan@jmurphy.me>
2018-04-05 16:26:54 -0400

    changed channel layers to remove routing

--- rc_server/settings.py
+++ rc_server/settings.py
@@ -63,11 +63,10 @@ redis_host = os.environ.get('REDIS_HOST', 'localhost')
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             'hosts': [(redis_host, 6379)],
         },
-        'ROUTING': 'rc_car.routing.channel_routing',
     }
 }
 
