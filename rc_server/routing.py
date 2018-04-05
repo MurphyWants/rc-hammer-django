4b4af75098b180c917d5780a84d49d63a3d881a0
jonathan murphy <jonathan@jmurphy.me>
2018-04-05 14:52:59 -0400

    chat tutorial http://channels.readthedocs.io/en/latest/tutorial/part_2.html

--- rc_server/routing.py
+++ rc_server/routing.py
@@ -1,20 +1,21 @@
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import rc_car.routing
-
 '''
 https://gearheart.io/blog/creating-a-chat-with-django-channels/
 https://codyparker.com/django-channels-with-react/2/
+http://channels.readthedocs.io/en/latest/tutorial/part_2.html
 '''
 
 # mysite/routing.py
 # http://channels.readthedocs.io/en/latest/tutorial/part_1.html
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket' : AuthMiddlewareStack(
+    'websocket': AuthMiddlewareStack(
         URLRouter(
-            rc_car.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns
         )
-    )
+    ),
 })
