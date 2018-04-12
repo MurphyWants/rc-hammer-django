6b5c935565437032797f2e2dc88c7ee4855b16f4
jonathan murphy <jonathan@jmurphy.me>
2018-04-12 15:23:37 -0400

    a fix for my fix

--- rc_car/views.py
+++ rc_car/views.py
@@ -26,7 +26,7 @@ def by_uuid(request, unique_id):
     try:
         rc_car = RC_Car.objects.get(pk=unique_id)
     except RC_Car.DoesNotExist:
-        return redirect('rc_car.views,index')
+        return redirect('/dashboard')
 
     current_user = request.user
     car_owner = rc_car.owner
