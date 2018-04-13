494de52b4df0211b68e9964b603794e9133e72fe
jonathan murphy <jonathan@jmurphy.me>
2018-04-12 20:40:03 -0400

    remove auth tokens for now

--- rc_car/urls.py
+++ rc_car/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('new_car', views.new_car, name='new_car'),
     path('rc/<uuid:unique_id>/edit/', views.edit_car, name='edit_car'),
     path('rc/<uuid:unique_id>/change_password/', views.change_password, name='change_password'),
-    url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
-    url(r'^rest-auth/', include('rest_auth.urls')),
     ]
