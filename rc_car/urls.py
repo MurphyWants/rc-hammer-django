0da1a507877d258aa0e0531d5424ae268bcd3e00
jonathan murphy <jonathan@jmurphy.me>
2018-04-12 20:18:27 -0400

    removed call to api, might do later

--- rc_car/urls.py
+++ rc_car/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('new_car', views.new_car, name='new_car'),
     path('rc/<uuid:unique_id>/edit/', views.edit_car, name='edit_car'),
     path('rc/<uuid:unique_id>/change_password/', views.change_password, name='change_password'),
-    path('api/owned_cars/', views.RC_Car_ViewSet_Owned_by, name='api_owned_cars'),
     ]
