43a92259376ee9b63d8f6889e0f8fddb2303b436
jonathan murphy <jonathan@jmurphy.me>
2018-04-12 20:14:42 -0400

    removed fields all, cant have fields and exclude

--- rc_car/serializers.py
+++ rc_car/serializers.py
@@ -4,5 +4,4 @@ from rest_framework import serializers
 class Rc_Car_Serializer(serializers.ModelSerializer):
     class Meta:
         model = RC_Car
-        fields = '__all__'
         exclude = ('password', 'current_user')
