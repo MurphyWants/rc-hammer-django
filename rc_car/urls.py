60580fe8d95494004247b06f9bcdb8f3256aa75c
jonathan murphy <jonathan@jmurphy.me>
2018-04-12 20:10:04 -0400

    more typo?

--- rc_car/urls.py
+++ rc_car/urls.py
@@ -4,7 +4,7 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'owned_car', views.RC_Car_ViewSet_Owned_by)
+router.register(r'owned_by', views.RC_Car_ViewSet_Owned_by)
 
 urlpatterns = [
     path('', views.index, name='index'),
