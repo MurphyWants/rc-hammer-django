f31fe1c3d962cf8de2bb755e93788684b289e7fc
jonathan murphy <jonathan@jmurphy.me>
2018-04-12 20:27:42 -0400

    typo

--- rc_car/models.py
+++ rc_car/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser
 from datetime import datetime
 from django.conf import settings
-from django.db.models.signal import post_save
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 import uuid
 
