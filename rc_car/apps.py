c8e5c70bc63dd0bfa6f0fda023ff27201b5a0772
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-14 16:02:33 -0400

    new ready function to reset current_user to none

--- rc_car/apps.py
+++ rc_car/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+from .models import RC_Car
 
 class RcCarConfig(AppConfig):
     name = 'rc_car'
+
+    def ready(self):
+        for car in RC_Car.objects.all():
+            car.current_user = None
+            print(car.id, " setting to None")
+            car.save()
