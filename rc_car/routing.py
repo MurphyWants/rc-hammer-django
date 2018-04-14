6929adc9ecab7d8d5dffe9f7cc9d66dcd92c379a
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-14 14:23:50 -0400

    removed consumer, forgot to remove from routing

--- rc_car/routing.py
+++ rc_car/routing.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/<unique_id>/video/', consumers.Video_Consumer),
     url(r'^ws/(?P<unique_id>[^/]+)/control/$', consumers.Drive_Consumer),
     url(r'^ws/<unique_id>/data/', consumers.Data_Consumer),
     url(r'^ws/(?P<unique_id>[^/]+)/in_use/$', consumers.In_Use_Consumer),
