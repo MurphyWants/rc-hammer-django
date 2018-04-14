d98759475382b5374f4e1e72a62dac570e37237c
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-14 16:06:32 -0400

    move the import to the function

--- rc_car/apps.py
+++ rc_car/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-from .models import RC_Car
 
 class RcCarConfig(AppConfig):
     name = 'rc_car'
 
     def ready(self):
+        from .models import RC_Car
         for car in RC_Car.objects.all():
             car.current_user = None
             print(car.id, " setting to None")
